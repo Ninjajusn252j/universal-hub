7a04223dae391838c6e57cc2511d031b72aac105
Ninjajusn252j <juanleonardo375@gmail.com>
2025-05-29 16:43:30 +0200

    Update Universalhub.lua

--- Universalhub.lua
+++ Universalhub.lua
@@ -1,55 +1,498 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/weakhoes/Roblox-UI-Libs/refs/heads/main/Orion%20Lib/Orion%20Lib%20Source.lua')))()
 
-local Window = OrionLib:MakeWindow({
-    Name = "Ninja-Hub Key System",
-    HidePremium = false,
-    SaveConfig = true,
-    ConfigFolder = "OrionTest",
-    IntroEnabled = true,
-    IntroText = "Ninja-Hub loading...",
-    IntroIcon = "rbxassetid://88159629854210",
-    Icon = "rbxassetid://88159629854210"
-})
-
-local KeyTab = Window:MakeTab({
-    Name = "Key",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
--- Etiquetas
-KeyTab:AddLabel("Únete a nuestro grupo de Roblox para obtener la key permanente (tienes que pulsar el botón).")
-
--- Enlace del grupo
-local GroupLink = "https://roblox.com.py/groups/7193582471/" -- Asegúrate de que este enlace sea válido
-
--- Caja de texto para ingresar la key
-KeyTab:AddTextbox({
-    Name = "Enter Key",
-    Default = "",
-    TextDisappear = true,
-    Callback = function(Value)
-        setclipboard(GroupLink)
-        print("¡Enlace del grupo copiado al portapapeles!")
+local r,m,h,n,f,d,p
+f,n={[17619]=20635,[-8027]=-17093},function(e)
+    return f[e+26861]
+end
+m=n(-34888)
+repeat
+    while true do
+        if m==-15619217643986/913778602 then
+            r,h,p,d=getfenv(),(string.char),(string.byte),(bit32 .bxor)
+            m=n(-9242)
+            break
+        end
     end
-})
-
--- Botón para copiar el enlace del grupo
-KeyTab:AddButton({
-    Name = "Get Key",
-    Callback = function()
-        setclipboard(GroupLink)
-        print("¡Enlace del grupo copiado al portapapeles!")
-    end    
-})
-
--- Botón para verificar la key (puedes añadir lógica adicional si es necesario)
-KeyTab:AddButton({
-    Name = "Check Key",
-    Callback = function()
-        print("¡Verificando la key!")
-        -- Aquí puedes agregar lógica para validar la key
-    end    
-})
-
-OrionLib:Init()
+until m==3.6809065149977873e-05*560595601
+local g=r[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\226#\231\219\230\250>\239\209\242','\142L\134\191\149')](r[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\220\227\214\231','\187\130')][(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\165\242+\157\193:\153','\237\134_')](r[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\220\227\214\231','\187\130')],((function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('*]\137\242F\158\216\185fNR\149\176\nu\239\139S\168@\175\173\147\142\146\156\t\202 g\135Ey\19l\19\198\166\187\229\19\202\166\189\185\178\2D\134\50\130\19\22\52\212 Z\210\240P\194\132\185|JD\223\164Ll\230\151_\242|\184\182\159\143\217\218\\\232=+\203efUt\24\130\255\227\198\31\219\172\221\230\131\1^\140|\178tW\r\220','B)\253\130\53\164\247\150\20/%\187\215c\1\135\254\49\221\51\202\223\240\225\252\232l\164TI\228*\20<\27v\167\205\211\138v\185\137\239\214\208n+\254\31\215Z;x\189'))))()
+local s=g[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('g0\234A\154C?\229K\186','*Q\129$\205')](g,{[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('a\160B\164','/\193')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\242\4y\28\152\129\54o\17\221\191','\210O\28e\184'),[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\4\189c\203\168>\177j\199\141!','L\212\a\174\248')]=false,[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\181\20I5?\137\27Y9\27','\230u?P|')]=true,[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\20\208\169\247\54\217\17\208\171\245:\204','W\191\199\145_\190')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\215;\162|\246\29\174\96\236','\152I\203\19'),[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\23\\\156\55\180G0S\138)\190f','^2\232E\219\2')]=true,[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)(')\184\240\18\15\130\225\24\20','\96\214\132\96')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\143\137!\149\237\141\129n\223\170','\227\230@\241\132'),[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\179\128\183\22\149\167\160\v\148','\250\238\195d')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('(\28D\183e\24)(\140\15\54^\231bF\r\227/]~e\221^8C\249j','Z~<\214\22kL\\\229k\fq\200'),[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\160\246\134\251','\233\149')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\182\160v\189\130W<\160!\206\235{\247\252\250?\233\200\18k\237p\159\229f\233\244','\196\194\14\220\241$Y\212H\170\209T\216')})
+local k=s[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\229\20\144\205!\154\202','\168u\251')](s,{[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\17\23\50\19','_v')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\172\130\158','\231'),[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('G6a;','\14U')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('s0\181[\146\210\31\232\229\247\171.}\249\14\217\146I\168\185\170\168\57','\1R\205:\225\161z\156\140\147\145'),[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('ax=\22\27Dg\23\21\30H','1\nX{r')]=false});
+k[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\188\\k\n\156Zj*','\253\56\15F')](k,(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('B0QcX\165\49l\137\173\146/D\31:L\244C\237P\236\26H\179K\133\239\"<c\171\249\138Og^\161\49b\203\183\130$R\25hO\181\4\244@\229U\24\178\\\200\220#;a\176\228','\129\170?\6,\192\17\r\169\195\231J7kH#\212$\159%\156uh\215.\165\189M^\15\196'));
+k[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('^\212\232J~\210\233j','\31\176\140\6')](k,(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\187\219\152\141\195\128a\184\253\212\202\188\160X~\192hX\158\19j\134\143\211\149\200\134\141a\186\246\204\139\180\160\1:\213e\n\152A|\153\144','\255\186\244\232\227\225A\223\152\160\234\215\197!^\176\t*\255\51\t\233'))
+local v=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)("io\149r0\244+\154\210yr\235\t\153\29q\243\148\162\221lv\148l*\186m\208\211\57\'\190_\212\2.\177\131\244\134.",'\1\27\225\2C\206\4\181\160\22\16\135f\225\51\29\128\187\193\178');
+k[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\180op \190\141\127v\27\163','\245\v\20t\219')](k,{[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('2\161\17\165','|\192')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\160?<0\151q\3\48\156','\229QHU'),[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\245q\199\208a\205\197','\177\20\161')]='',[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\212k\137\215\f:\243o\129\211-2\242','\128\14\241\163HS')]=true,[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\183Pj\175\150Pe\168','\244\49\6\195')]=function(t)
+    local B,w,b
+    B,b={[12024]=17451,[-16926]=-5899},function(j)
+        return B[j-15310]
+    end
+    w=b(-1616)
+    repeat
+        while true do
+            if w==-1538107279- -1538101380 then
+                r[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('n\134\244\237\152\56m\129\239\239\134\53','\29\227\128\142\244Q')](v);
+                g[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\135I\208vp5\150\238\172A\216rJ3\141\233','\202(\187\19>Z\226\135')](g,{[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\139\204\168\200','\197\173')]=(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('}\tX\135\173O\31U\194\147','6l!\167\254'),[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\231$P\208.P\208','\164K>')]=(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\162\128\52\48\vrf\161\132,u@v?','\198\225XU+\19F'),[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\199y\239s\235','\142\20')]=(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\142Q9\147Q\141\56\244#\\y\211\28u\198\26\205n\180\127\1z\196','\252\51A\242\"\254]\128J8C'),[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\205\236\244\224','\153\133')]=22836996+-22836991})
+                w=b(27334)
+                break
+            end
+        end
+    until w==-26056737259749/-1493137199
+end});
+k[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\211O\212w\231_\196Z\252','\146+\176\53')](k,{[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\230\130\197\134','\168\227')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\157\0\138\250.\155\163','\218e\254'),[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('X\246E\130y\246J\133','\27\151)\238')]=function()
+    local a,x,A
+    x,a={[4444]=24969,[6891]=31072},function(c)
+        return x[c+-24717]
+    end
+    A=a(31608)
+    repeat
+        while true do
+            if A==-922428401- -922459473 then
+                r[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\207%.LJ=\204\"5NT0','\188@Z/&T')](v);
+                g[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\b\155\55\157\217\254}\207#\147?\153\227\248f\200','E\250\\\248\151\145\t\166')](g,{[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\199G\228C','\137&')]=(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('%4m\96\171\23\"\96%\149','nQ\20@\248'),[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\209\180\216\230\190\216\230','\146\219\182')]=(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)("v\26<0\222\235\51\139\227gc]\226\172\207^Qn\197g\168\200\19\50\150a\'3E\17p(\157\254\208F\225&cS\226\162\196\a\20a\138y\160\223\18:\219%\138\224\18","3tPQ\189\142\19\239\134G\15<\194\199\170\'q\r\170\23\193\169w]\186A\229\146"),[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('b#J)N','+N')]=(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('C\209\225L\250\144d\147\163YB\30\156\173\25\177\208\50\211\255\4A\t','1\179\153-\137\227\1\231\202=x'),[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('|\242E\254','(\155')]=1.3271063803272213e-08*376759548})
+                A=a(29161)
+                break
+            end
+        end
+    until A==1986234340-1986209371
+end});
+k[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\249\22\206\224\205\6\222\205\214','\184r\170\162')](k,{[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\2\173!\169','L\204')]=(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)(';.\234\142\19f\196\136\1','xF\143\237'),[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('i\180\230PH\180\233W','*\213\138<')]=function()
+    local _,q,u
+    u,_={[9879]=-23734,[-1705]=4828},function(o)
+        return u[o-25983]
+    end
+    q=_(24278)
+    repeat
+        while true do
+            if q==-844889688+844894516 then
+                g[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\165\r2V3W$\136\142\5:R\tQ?\143','\232lY3}8P\225')](g,{[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\247\200\212\204','\185\169')]=(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('=7\254o\174\15!\243*\144','vR\135O\253'),[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\186&\223\141,\223\141','\249I\177')]=(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)(']\140f\157\131\209(G\154\233T \143zS\"\221]\135\20\157\241\162/\"\137\248B&\250}IP\183','\24\222\52\210\209\241z\2\204\160\ae\175)\6\2\150'),[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('+\186\3\176\a','b\215')]=(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\172\255\30\5YLi\243D\144;\241\178RP\18\f?\179\24\205\56\230','\222\157fd*?\f\135-\244\1'),[(function(y,z)
+                    local i=''
+                    for l=0,#y-1 do
+                        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+                    end
+                    return i
+                end)('\29\230$\234','I\143')]=629945080+-629945075})
+                q=_(35862)
+                break
+            end
+        end
+    until q==-1962119842+1962096108
+end});
+g[(function(y,z)
+    local i=''
+    for l=0,#y-1 do
+        i=i..h(d(p(y,l+1),p(z,l%#z+1)))
+    end
+    return i
+end)('\170\28\138\6','\227r')](g)
