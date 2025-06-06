b3921b90e9373859915c88f5589fe6d0e6ebfca2
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 21:05:56 +0200

    Update Universalhub.lua

--- Universalhub.lua
+++ Universalhub.lua
@@ -1,4 +1,7 @@
--- Key System Script by DarkEclipse252j--
+-- Contenido de https://raw.githubusercontent.com/Ninjajusn252j/scripts-discord/main/scripts/gonzalo01034.lua
+
+-- No necesitas definir 'GroupLink' aquí, ya que se toma de _G.Config.GroupLink
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/weakhoes/Roblox-UI-Libs/refs/heads/main/Orion%20Lib/Orion%20Lib%20Source.lua')))()
 
 local Window = OrionLib:MakeWindow({
@@ -27,10 +30,10 @@ KeyTab:AddTextbox({
     Default = "",
     TextDisappear = true,
     Callback = function(Value)
-        setclipboard(GroupLink)
+        setclipboard(_G.Config.GroupLink) -- ¡Accede a GroupLink desde _G.Config!
         OrionLib:MakeNotification({
             Name = "Key System",
-            Content = "dale a get key",
+            Content = "Dale a get key",
             Image = "rbxassetid://4483345998",
             Time = 5
         })
@@ -41,7 +44,7 @@ KeyTab:AddTextbox({
 KeyTab:AddButton({
     Name = "Get Key",
     Callback = function()
-        setclipboard(GroupLink)
+        setclipboard(_G.Config.GroupLink) -- ¡Accede a GroupLink desde _G.Config aquí también!
         OrionLib:MakeNotification({
             Name = "Key System",
             Content = "Enlace de la key copiado, ¡ve y pégalo en el navegador!",
@@ -50,7 +53,6 @@ KeyTab:AddButton({
         })
     end
 })
-
 
 KeyTab:AddButton({
     Name = "Check Key",
