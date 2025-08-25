1ed1576b60bad61699df3fc7d15e279b86330676
DarkEclipse252j <juanleonardo375@gmail.com>
2025-08-25 10:32:37 +0200

    Update Universalhub.lua

--- Universalhub.lua
+++ Universalhub.lua
@@ -1,6 +1,6 @@
 local Key = "Imposiblehahaha"
 
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/Rayfield/Rayfield/main/source"))()
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Acceso Requerido",
